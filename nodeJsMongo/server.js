ac55e412118a727ecfcc0f2eb22cdbd0b53148dd
“Phupa111” <santapupa111@gmail.com>
2023-11-04 00:15:28 +0700

    commit3

--- nodeJsMongo/server.js
+++ nodeJsMongo/server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const mariadb = require('mariadb');
 const { type } = require('os');
@@ -41,6 +42,67 @@ app.get('/sushi', async (req, res) => {
         }
     }
 });
+
+app.get('/bill/:id', async (req, res) => {
+    const userId = req.params.id; // Capture the user's ID from the URL parameter
+    
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query('SELECT `total_price`, `id`, `uid` FROM `sushi`.`bill` WHERE  `uid` = ?', [userId]);
+
+        // Convert BigInt values to regular numbers
+        const formattedRows = rows.map(row => {
+            return {
+                id: Number(row.id),
+                total_price: row.total_price,
+                uid: Number(row.uid)
+            };
+        });
+
+        // Send the response as JSON
+        res.status(200).json(formattedRows);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) {
+            conn.release(); // Release the database connection back to the pool
+        }
+    }
+});
+
+
+app.post('/bill/insert', async (req, res) => {
+    const { uid } = req.body; // Capture the user ID from the request body
+    console.log(uid);
+    if (uid === undefined || uid === null) {
+        res.status(400).json({ error: 'Invalid user ID' });
+        return;
+    }
+
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const result = await conn.query('INSERT INTO `sushi`.`bill` (`total_price`, `uid`) VALUES (0, ?)', [uid]);
+
+        // Send the response as JSON
+        const formattedResult = {
+            id: Number(result.insertId),
+            total_price: 0,
+            uid: Number(uid),
+        };
+        res.status(200).json(formattedResult);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        if (conn) {
+            conn.release(); // Release the database connection back to the pool
+        }
+    }
+});
+
 
 
 
